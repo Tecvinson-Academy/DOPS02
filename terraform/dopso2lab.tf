2c1f117627299ab2427aaf2793b94f0344e4502a
igho347 <igho_igbini@yahoo.com>
2024-12-15 10:24:02 -0400

    chore: terraform code fix

--- terraform/dopso2lab.tf
+++ terraform/dopso2lab.tf
@@ -5,15 +5,15 @@ provider "aws" {
 
 # Define variables for the vpc
 variable "vpc_id" {
-  default = "vpc-08fcbd2bedfbbddaa" 
-  }
+  default = "vpc-08fcbd2bedfbbddaa"
+}
 
 variable "db_password" {
-    }
+}
 
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id            = "vpc-08fcbd2bedfbbddaa" 
+  vpc_id            = "vpc-08fcbd2bedfbbddaa"
   cidr_block        = "192.168.192.0/19"
   availability_zone = "us-east-1a"
 
@@ -23,7 +23,7 @@ resource "aws_subnet" "private_subnet" {
 }
 
 resource "aws_subnet" "private_subnet01" {
-  vpc_id            = "vpc-08fcbd2bedfbbddaa" 
+  vpc_id            = "vpc-08fcbd2bedfbbddaa"
   cidr_block        = "192.168.224.0/19"
   availability_zone = "us-east-1b"
 
@@ -33,14 +33,14 @@ resource "aws_subnet" "private_subnet01" {
 }
 
 resource "aws_db_instance" "db_instance" {
-  identifier             = "db_instance"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  engine                 = "mysql"
-  engine_version         = "14.1"
-  username               = "admin"
-  password               = var.db_password
-  skip_final_snapshot    = true
+  identifier          = "db_instance"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 5
+  engine              = "mysql"
+  engine_version      = "14.1"
+  username            = "admin"
+  password            = var.db_password
+  skip_final_snapshot = true
 }
 
 resource "aws_ecr_repository" "ecr" {
@@ -60,7 +60,7 @@ resource "aws_ecr_repository" "ecr" {
 resource "aws_ecr_repository" "ecr01" {
   name                 = "dopso2-be"
   image_tag_mutability = "MUTABLE"
-git 
+  image_scanning_configuration {
     scan_on_push = true
   }
 
@@ -69,14 +69,13 @@ git
     Name        = "dopso2-be"
   }
 }
-
 terraform {
 
   backend "s3" {
 
     bucket = "dops02-terraform"
 
-    key    = "state-file-folder"
+    key = "state-file-folder"
 
     region = "us-east-1"
 
